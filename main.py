44b078f201138a8f6b9efe13ee70bece26ca5d43
karimx17 <kareem.alkhatib@hotmail.com>
2022-02-15 11:57:13 -0700

    Add comment for recruiters

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from stocks import Polygon
 from scrapping import Scrapper
 
+# IF YOU ARE A RECRUITER AND ARE NOT FAMILIAR WITH PYTHON, SHOW THIS TO A SOFTWARE DEVELOPER AND THEY CAN RUN IT
+
+
 stocks = Polygon()
 stocks.percent_change()
 
